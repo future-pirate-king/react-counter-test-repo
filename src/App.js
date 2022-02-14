4227e47659ea6302edf7f9e40baa02c7064b27f2
Sam David <32875617+future-pirate-king@users.noreply.github.com>
2022-02-14 17:08:26 +0530

    running counter app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
+
+const CounterStatus = {
+  STOPPED: "STOPPED",
+  RUNNING: "RUNNING",
+  PAUSED: "PAUSED"
+};
+
+const styles = {
+  button: {
+    margin: 8
+  }
+};
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [counterStatus, setCounterStatus] = useState(CounterStatus.STOPPED);
+
+  useEffect(() => {
+    let interval;
+
+    if (counterStatus === CounterStatus.RUNNING) {
+      interval = setInterval(() => {
+        setCount((count) => count + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [counterStatus]);
+
+  const handleCounterStart = () => {
+    setCounterStatus(CounterStatus.RUNNING);
+  };
+
+  const handleCounterPause = () => {
+    setCounterStatus(CounterStatus.PAUSED);
+  };
+
+  const handleCounterReset = () => {
+    setCounterStatus(CounterStatus.STOPPED);
+    setCount(0);
+  };
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Counter app</h1>
+
+      <hr />
+
+      <h4>{count}</h4>
+
+      <div>
+        {(counterStatus === CounterStatus.STOPPED ||
+          counterStatus === CounterStatus.PAUSED) && (
+          <button style={styles.button} onClick={handleCounterStart}>
+            Start
+          </button>
+        )}
+
+        {counterStatus === CounterStatus.RUNNING && (
+          <button style={styles.button} onClick={handleCounterPause}>
+            Pause
+          </button>
+        )}
+
+        {counterStatus === CounterStatus.PAUSED && (
+          <button style={styles.button} onClick={handleCounterReset}>
+            Reset
+          </button>
+        )}
+      </div>
     </div>
   );
 }
